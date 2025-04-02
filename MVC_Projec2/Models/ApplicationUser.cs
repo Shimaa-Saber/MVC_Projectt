77ccc4d6fbd715bcb24db2688f3b3888c8dd5c5a
Shimaa-Saber <shimaasaber812@gmail.com>
2025-04-02 13:45:13 +0200

    finish register and login code

---
+++ MVC_Projec2/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MVC_Projec2.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string FullName { get; set; }
+        public DateTime CreatedAt { get; set; } 
+    }
+}
